9450d033317155f28a8192dd2a2fe2bb87f378a7
Achal Jhawar <>
2021-04-09 17:43:38 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+app.use('/css' , express.static(__dirname + 'public/css'))
+app.use('/js' , express.static(__dirname + 'public/js'))
+app.use('/icons' , express.static(__dirname + 'public/icons'))
+
+
+app.set('views' , './views')
+app.set('view engine' , 'ejs')
+
+app.get('',(req, res) => {
+  res.render('index')
+})
+app.get('/about',(req, res) => {
+  res.render('about')
+})
+app.get('/contact',(req, res) => {
+  res.render('contact')
+})
+
+
+app.listen(port , () => console.info(`Listening and port ${port}`))
